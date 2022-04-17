1bb7976c28fd2a70091ce78d9f8c117ec13a64d0
hüseyin cangür <huseyincangur35@gmail.comn>
2022-04-17 13:14:22 +0300

    Add project files.

---
+++ Signalr.Server/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Signalr.Server
+{
+    public class ChatHub:Hub
+    {
+        private static List<string> connectionList = new List<string>();
+        public override Task OnConnectedAsync()
+        {
+            string connectionId=Context.ConnectionId;
+            if(!connectionList.Any(x=>x==connectionId))
+                connectionList.Add(connectionId);
+            Clients.All.SendAsync("HasNewConnection", connectionList.Count);
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            string connectionId = Context.ConnectionId;
+            if (connectionList.Any(x => x == connectionId))
+                connectionList.Remove(connectionId);
+            Clients.All.SendAsync("HasNewConnection", connectionList.Count);
+            return base.OnDisconnectedAsync(exception);
+        }
+        public async Task PublicMessage(string message)
+        {
+            string connectionId = Context.ConnectionId;
+            await Clients.All.SendAsync("HasNewMessage", connectionId, message);
+        }
+    }
+}
---
+++ Signalr.WinClientForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Signalr.WinClientForm
+{
+    public partial class Form1 : Form
+    {
+        HubConnection hubConnection = new HubConnectionBuilder().WithAutomaticReconnect().WithUrl("http://localhost:50696/ChatHub").Build();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            hubConnection.StartAsync();
+
+            hubConnection.On<int>("HasNewConnection", (connectionCount) =>
+            {
+                label1.Text = $"Toplam Bağlantı Sayısı : {connectionCount}";
+            });
+            hubConnection.On<string, string>("HasNewMessage", (connectionId, message) =>
+            {
+                textBox1.AppendText($"{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}{connectionId} tarafından yazıldı :{message}");
+            });
+            hubConnection.StartAsync();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hubConnection.SendAsync("PublicMessage", textBox2.Text);
+            textBox2.Text = string.Empty;
+        }
+    }
+}
